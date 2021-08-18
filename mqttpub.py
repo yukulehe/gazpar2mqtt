7580c07b33cf6c81136ff7421e05ee8a6e974b27
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-08-18 17:34:57 +0200

    Update mqttpub.py

--- mqttpub.py
+++ mqttpub.py
@@ -32,7 +32,7 @@ client.on_disconnect = mqttutil.on_disconnect
 
 # Connect
 print("Connexion to broker")
-client.connect(util.mqtt_host["hostname"], util.mqtt_host["port"], 60)
+client.connect(mqttutil.mqtt_host["hostname"], mqttutil.mqtt_host["port"], 60)
 
 # Publish
 if mqtt_connected :
