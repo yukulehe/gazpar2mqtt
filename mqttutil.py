9500f37d6acf0557e272c4919a04fccde99ba0cf
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-08-18 11:02:45 +0200

    Create mqttutil.py

---
+++ mqttutil.py
@@ -0,0 +1,15 @@
+mqtt_host = dict({ "hostname": "test.mosquitto.org", "port": 1883 })
+topic_name = "world"
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print("Unexpected disconnection: {}".format(rc) )
+        
+def on_message(client, userdata, msg):
+    print("onMessageArrived: " + msg.topic + " " + str(msg.payload))
+    
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed: mid=" + str(mid) + " QoS=" + str(granted_qos))
+    
+def on_publish(client, userdata, mid):
+    print("Published: mid=" + str(mid) )
