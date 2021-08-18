a4b3d283df8dfb6eb9c939c998c5a89c0d6aaeba
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-08-18 18:49:03 +0200

    Update gazpar2mqtt.py

--- gazpar2mqtt.py
+++ gazpar2mqtt.py
@@ -1,1 +1,25 @@
-# Work in progress
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Work in progress...
+
+import os
+import sys
+import datetime
+#import schedule
+import time
+import locale
+from dateutil.relativedelta import relativedelta
+import gazpar
+import mqttpub
+import json
+
+import argparse
+import logging
+import pprint
+from envparse import env
+
+
+PFILE = "/.params"
+DOCKER_MANDATORY_VARENV=['GRDF_USERNAME','GRDF_PASSWORD','MQTT_HOSTNAME','MQTT_PORT']
+DOCKER_OPTIONAL_VARENV=['MQTT_CLIENTID','MQTT_QOS', 'MQTT_TOPIC', 'MQTT_RETAIN']
