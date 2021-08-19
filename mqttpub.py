65b79349955e8ca615a382fdf1eeeff1f4114729
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-08-19 10:21:06 +0200

    Update mqttpub.py

--- mqttpub.py
+++ mqttpub.py
@@ -14,35 +14,46 @@ def on_connect(client, userdata, flags, rc):
     print("on_connect: " + mqtt.connack_string(rc))
     mqtt_connected = True
 
-# ----------------------------
-# Main
-# ----------------------------
+# Sub create client
+def create_client(clientId)
+    
+    # Create instance
+    print("Instance mqtt client")
+    client = mqtt.Client(clientId)
+    
+    return client
 
-# Create instance
-print("Create mqtt instance")
-client = mqtt.Client(broker_client)
-
-# Activate callbacks
-client.on_connect = on_connect
-client.on_message = mqttutil.on_message
-client.on_publish = mqttutil.on_publish
-client.on_disconnect = mqttutil.on_disconnect
-
-# Connect
-print("Connexion to broker")
-client.connect(mqttutil.mqtt_host["hostname"], mqttutil.mqtt_host["port"], 60)
-time.sleep(2)
-client.loop_start()
-
-# Publish
-if mqtt_connected :
-    print("Publication to broker")
-    client.publish(mqttutil.topic_name, "Hello world")
-    time.sleep(3)
-
-# Stop loop
-client.loop_stop()
-
-# Disconnect
-print("Disconnexion")
-client.disconnect
+# Sub connect
+def connect(client,host,port):
+    
+    # Activate callbacks
+    print("Activate callback")
+    client.on_connect = on_connect
+    client.on_message = mqttutil.on_message
+    client.on_publish = mqttutil.on_publish
+    client.on_disconnect = mqttutil.on_disconnect
+    
+    # Connect
+    print("Connexion to broker")
+    client.connect(host,port, 60)
+    time.sleep(2)
+    
+    # Start loop
+    client.loop_start()
+    
+# Sub disconnect
+def disconnect(client):
+    
+    # End loop
+    client.loop_stop()
+    
+    # Disconnect
+    print("Disconnexion")
+    client.disconnect
+  
+# Sub publish
+def publish(client,topic,payload,qos,retain)
+    
+    print("Publish payload")
+    client.publish(topic, payload=payload, qos=qos, retain=retain)
+    time.sleep(1)
