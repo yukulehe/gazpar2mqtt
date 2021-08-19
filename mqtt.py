799f078cbcc5a833f7c7ff5732c3b617622dff31
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-08-19 17:09:40 +0200

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -2,15 +2,9 @@ import paho.mqtt.client as mqtt
 import time
 import logging
 
-# Broker settings
-broker_client = "gazou"
-
-# Initialize variables
-mqtt_connected = False
-
 # Callback on_connect
 def on_connect(client, userdata, flags, rc):
-    logging.info("Mqtt on_connect : %s", mqtt.connack_string(rc))
+    logging.debug("Mqtt on_connect : %s", mqtt.connack_string(rc))
 
 # Callback on_disconnect
 def on_disconnect(client, userdata, rc):
@@ -19,7 +13,7 @@ def on_disconnect(client, userdata, rc):
 
 # Callback on_publish
 def on_publish(client, userdata, mid):
-    logging.info("Mqtt on_publish : message published")
+    logging.debug("Mqtt on_publish : message published")
 
 # Sub constructor
 def create_client(clientId):
@@ -32,13 +26,13 @@ def create_client(clientId):
 def connect(client,host,port):
     
     # Activate callbacks
-    logging.info("Mqtt connect : activation of callbacks")
+    logging.debug("Mqtt connect : activation of callbacks")
     client.on_connect = on_connect
     client.on_publish = on_publish
     client.on_disconnect = on_disconnect
     
     # Connect
-    logging.info("Mqtt connect : connection to broker...")
+    logging.debug("Mqtt connect : connection to broker...")
     client.connect(host,port, 60)
     time.sleep(2)
     
@@ -53,12 +47,12 @@ def disconnect(client):
     client.loop_stop()
     
     # Disconnect
-    logging.info("Mqtt disconnect : disconnection...")
+    logging.debug("Mqtt disconnect : disconnection...")
     client.disconnect
   
 # Sub publish
 def publish(client,topic,payload,qos,retain):
     
-    logging.info("Mqtt publish : publication...")
+    logging.debug("Mqtt publish : publication...")
     client.publish(topic, payload=payload, qos=qos, retain=retain)
     time.sleep(1)
