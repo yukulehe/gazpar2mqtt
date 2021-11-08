eff0b3f7efdcb086001cef46fab65cee8e8fbd73
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-11-08 18:58:54 +0100

    Update hass.py

--- app/hass/hass.py
+++ app/hass/hass.py
@@ -12,74 +12,51 @@ HASS_DEVICE_CLASS_ENERGY = "energy"
 HASS_UNIT_CLASS_GAS = "m3"
 HASS_UNIT_CLASS_ENERGY = "kWh"
 
-
-@dataclass
-class EntityDescription:
-    """Define a description (set of characteristics) of a Home Assistant entity."""
-
-    platform: str
-
-    device_class: Optional[str] = None
-    icon: Optional[str] = None
-    unit: Optional[str] = None
-    unit_class: Optional[str] = None
+HA_AUTODISCOVERY_PREFIX = "homeassistant"
 
 
-ENTITY_DESCRIPTIONS = {
-    GASPAR_GAS: EntityDescription(
-        platform=HASS_COMPONENT_SENSOR,
-        device_class=HASS_DEVICE_CLASS_GAS,
-        unit_class=HASS_UNIT_CLASS_GAS,
-    ),
-    GASPAR_ENERGYY: EntityDescription(
-        platform=HASS_COMPONENT_SENSOR,
-        device_class=HASS_DEVICE_CLASS_ENERGY,
-        unit="HASS_UNIT_CLASS_ENERGY",
-    ),
-}
 
 def getHassStateTopic(device):
     
-    topic = f"{ha_autodiscovery_prefix}/{ha_device_class_gas}/gazpar/state"
+    topic = f"{HA_AUTODISCOVERY_PREFIX}/{HASS_COMPONENT_SENSOR}/gazpar/state"
     return topic
 
-def getHassTopic(device):
+def getHassConfigTopic():
     
-    if device == gas:
-        topic = f"{ha_autodiscovery_prefix}/{ha_device_class_gas}/gazpar/state"
-    elif device == energy:
-        topic = f"{ha_autodiscovery_prefix}/{ha_device_class_gas}/gazpar/state"
-    else:
-        topic = "error"
+    configTopic = f"{HA_AUTODISCOVERY_PREFIX}/{HASS_COMPONENT_SENSOR}/gazpar/config"
+    return configTopic
     
-    return topic
     
-
+def getHassConfigPayload(device):
     
-def getHassConfig(device,value):
+    # Set state topic
+    state_topic = f"{HA_AUTODISCOVERY_PREFIX}/sensor/gazpar/state"
     
+    # Gas consumption daily
     if device == daily_gas:
         
         config= {
             "device_class" : "gas"
             "name" : "gaspar_daily_gas"
             "unique_id" : "gaspar_daily_gas"
-            "state_topic" = getHassStateTopic(device)
+            "state_topic" = state_topic
             "unit_of_measurement" : HASS_UNIT_CLASS_GAS
             "value_template" : "{{ value_json.daily_gas}}"
         }
     
+    # Gas consumption monthly
     elif device == monthly_gas:
         
         config= {
             "device_class" : "gas"
             "name" : "gaspar_monthly_gas"
             "unique_id" : "gaspar_monthly_gas"
-            "state_topic" = getHassStateTopic(device)
+            "state_topic" = state_topic
             "unit_of_measurement" : HASS_UNIT_CLASS_GAS
             "value_template" : "{{ value_json.monthly_gas}}"
         }
-        
+    
+    # Energy consumption daily
     elif device == daily_energy:
         
         config= {
@@ -90,7 +67,8 @@ def getHassConfig(device,value):
             "unit_of_measurement" : HASS_UNIT_CLASS_ENERGY
             "value_template" : "{{ value_json.daily_energy}}"
         }
-     
+    
+    # Energy consumption monthly
     elif device == monthly_energy:
         
         config= {
