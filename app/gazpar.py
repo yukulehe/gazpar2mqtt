4f41922bac770be834f7f076c454a441693b9b7f
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-11-18 11:30:47 +0100

    Update gazpar.py

--- app/gazpar.py
+++ app/gazpar.py
@@ -362,7 +362,7 @@ def _get_data(session, resource_id, start_date=None, end_date=None):
         rdate = ts[i].replace('Le ','').replace('/','-')
         for d in data:
             if rdate == d['date']:
-                d['mcube'] = float(ds[i])
+		d['mcube'] = float(ds[i])
 		d['mcube_seuil'] = float(ss[i])
 		d['mcube_prec'] = float(ps[i])
 		
