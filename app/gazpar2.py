b7f75c52ba97abefe0b291fdfb7906e49f7be1ca
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-11-24 14:57:56 +0100

    Update gazpar2.py

--- app/gazpar2.py
+++ app/gazpar2.py
@@ -9,6 +9,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--pygazpar-login", dest="PYGAZPAR_LOGIN", help="pygazpar login")
 parser.add_argument("--pygazpar-password", dest="PYGAZPAR_PASSWORD", help="pygazpar password")
 
+args = parser.parse_args()
+
+log = logging.getLogger()
+logging.basicConfig(stream=sys.stderr, level=logging.WARNING,
+                    format='%(name)s (%(levelname)s): %(message)s')
+log.setLevel(max(3 - args.verbose_count, 0) * 10)
+
 client = pygazpar.Client(args.PYGAZPAR_LOGIN,
                          args.PYGAZPAR_PASSWORD,
                          'geckodriver',
