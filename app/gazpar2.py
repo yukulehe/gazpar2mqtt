8cac90c410e717dffb9c3431d48bd370bfa7584d
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-11-27 20:26:36 +0100

    Update gazpar2.py

--- app/gazpar2.py
+++ app/gazpar2.py
@@ -5,27 +5,50 @@ import logging
 import argparse
 import pygazpar
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--pygazpar-login", dest="PYGAZPAR_LOGIN", help="pygazpar login")
-parser.add_argument("--pygazpar-password", dest="PYGAZPAR_PASSWORD", help="pygazpar password")
 
-args = parser.parse_args()
+def login(username, password):
 
-log = logging.getLogger()
-logging.basicConfig(stream=sys.stderr, level=logging.WARNING,
-                    format='%(name)s (%(levelname)s): %(message)s')
+    """Logs the user into the Linky API.
+    """
+    global JAVAVXS
+    session = requests.Session()
 
-client = pygazpar.Client(args.PYGAZPAR_LOGIN,
-                         args.PYGAZPAR_PASSWORD,
-                         '/usr/local/bin/geckodriver',
-                         30,
-                         '/tmp')
+    session.headers = {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Mobile Safari/537.36',
+        'Accept-Encoding':'gzip, deflate, br',
+        'Accept':'application/json, */*',
+        'Connection': 'keep-alive'
+    }
 
-log.debug('Starting to update pygazpar data')
-client.update()
-log.debug('End update pygazpar data')
+    #Get essential cookies
+    req = session.get('https://monespace.grdf.fr/client/particulier/accueil')
+    if not 'auth_nonce' in session.cookies:
+        raise GazparLoginException("Cannot get auth_nonce.")
+    auth_nonce = session.cookies.get('auth_nonce')
+    logging.info("auth_nonce: " + auth_nonce)
 
-data = client.data()
+    payload = {
+        'email': '***MAIL***',
+        'password': '***PASS***',
+        'capp': 'meg',
+        'goto': 'https://sofa-connexion.grdf.fr:443/openam/oauth2/externeGrdf/authorize?response_type=code&scope=openid%20profile%20email%20infotravaux%20%2Fv1%2Faccreditation%20%2Fv1%2Faccreditations%20%2Fdigiconso%2Fv1%20%2Fdigiconso%2Fv1%2Fconsommations%20new_meg%20%2FDemande.read%20%2FDemande.write&client_id=prod_espaceclient&state=0&redirect_uri=https%3A%2F%2Fmonespace.grdf.fr%2F_codexch&nonce=' + auth_nonce + '&by_pass_okta=1&capp=meg'
+    }
 
-for measure in data:
-    print(measure['volume_m3'])
+    req = session.post('https://login.monespace.grdf.fr/sofit-account-api/api/v1/auth', data=payload, allow_redirects=False)
+    if not 'XSRF-TOKEN' in session.cookies:
+        raise GazparLoginException("Login unsuccessful. Check your credentials.")
+
+    req = session.get('https://sofa-connexion.grdf.fr:443/openam/oauth2/externeGrdf/authorize?response_type=code&scope=openid%20profile%20email%20infotravaux%20%2Fv1%2Faccreditation%20%2Fv1%2Faccreditations%20%2Fdigiconso%2Fv1%20%2Fdigiconso%2Fv1%2Fconsommations%20new_meg%20%2FDemande.read%20%2FDemande.write&client_id=prod_espaceclient&state=0&redirect_uri=https%3A%2F%2Fmonespace.grdf.fr%2F_codexch&nonce=' + auth_nonce + '&by_pass_okta=1&capp=meg')
+
+    req = session.get('https://monespace.grdf.fr/api/e-connexion/users/whoami')
+
+    req = session.get('https://monespace.grdf.fr/api/e-conso/pce')
+    logging.info("PCEs:")
+    logging.info(req.text)
+  
+    req = session.get('https://monespace.grdf.fr/api/e-conso/pce/consommation/informatives?dateDebut=2018-11-27&dateFin=2021-11-27&pceList%5B%5D=**PCE***')
+    logging.info("CONSO:")
+    logging.info(req.text)
+
+
+    return session
