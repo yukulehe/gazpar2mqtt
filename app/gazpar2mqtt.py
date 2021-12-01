e9f0a0b59167e2ea53621c5e26084743df9b0ae0
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-12-01 14:46:53 +0100

    Update gazpar2mqtt.py

--- app/gazpar2mqtt.py
+++ app/gazpar2mqtt.py
@@ -357,7 +357,7 @@ def run(params):
 
             # Publish config
             for myEntity in myDevice.entityList:
-                print(myEntity.id,myEntity.configTopic," : ",myEntity.getConfigPayloadJson)
+                print(myEntity.id + myEntity.configTopic + " : " + myEntity.getConfigPayloadJson)
                 #myMqtt.publish(myEntity.configTopic, myEntity.getConfigPayloadJson)
 
             # Publish state of all entities of the device
