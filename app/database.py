772a128e836d33809a46b8a3e09f2e2a48440845
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-12-02 18:52:08 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -4,3 +4,20 @@ import sys
 import logging
 import datetime
 import sqlite3
+
+
+class database:
+  
+    def __init__(self):
+        self.isConnected = False
+        self.con = None
+        self.cur = None
+    
+  
+    def connect(self,database):
+        logging.debug("Connexion to sqlite database...")
+        self.con = sqlite3.connect(database)
+        wait(3)
+        logging.debug("Get database cursor...")
+        self.cur = self.con.cursor()
+        
