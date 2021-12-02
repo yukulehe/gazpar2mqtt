b669754f06ca566ae2a40eb35ab9cb31a7064384
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-12-02 19:35:19 +0100

    Update gazpar.py

--- app/gazpar.py
+++ app/gazpar.py
@@ -38,9 +38,9 @@ def _convertGrdfDate(date):
 
 
 #######################################################################
-#### Class GRDF Web
+#### Class GRDF
 #######################################################################
-class GrdfWeb:
+class Grdf:
     
     # Constructor
     def __init__(self):
@@ -171,7 +171,6 @@ class GrdfWeb:
         pce.dailyMeasureStart = startDate
         pce.dailyMeasureEnd = endDate
         
-        # Loop on measure list
         for measure in measureList[pce.pceId]["releves"]:
             
             # Create the measure
@@ -180,38 +179,7 @@ class GrdfWeb:
             # Append measure to the PCE's measure list
             pce.addDailyMeasure(myDailyMeasure)
             
-
             
-#######################################################################
-#### Class GRDF Database
-#######################################################################
-class GrdfDb:
-    
-    # Constructor
-    def __init__(self,databaseFile):
-        
-        # Database attributes
-        self.isConnected = False
-        self.con = None
-        self.cur = None
-        
-        # GRDF attributes
-        self.whoiam = None
-        self.pceList = []
-    
-    # Connect to database
-    def connect(self,databaseFile):
-        
-        # implement code here
-        # self.cur = ...
-        # self.cur = ...
-        
-    # Implement other methods here
-    # def ...(self):
-
-    
-    
-    
 
 #######################################################################
 #### Class Account
