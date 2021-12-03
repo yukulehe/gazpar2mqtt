eafa607dbbc748fd3b0598f718bdb27fbe82bf82
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-12-03 16:12:55 +0100

    Update standalone.py

--- app/standalone.py
+++ app/standalone.py
@@ -1,3 +1,29 @@
 ### Define Standalone functionality. ###
 
+# Constants for topics
 
+## Daily
+TOPIC_DAILY_DATE = "/daily/date"
+TOPIC_DAILY_KWH = "/daily/kwh"
+TOPIC_DAILY_MCUBE = "/daily/mcube"
+
+## Monthly
+TOPIC_MONTHLY_DATE = "/monthly/month"
+TOPIC_MONTHLY_KWH = "/monthly/kwh"
+TOPIC_MONTHLY_KWH_TSH = "/monthly/kwh/threshold"
+TOPIC_MONTHLY_KWH_PREV = "/monthly/kwh/previous"
+TOPIC_MONTHLY_MCUBE = "/monthly/mcube"
+TOPIC_MONTHLY_MCUBE_PREV = "/monthly/kwh/previous"
+
+
+## Status
+TOPIC_STATUS_DATE = "/status/date"
+TOPIC_STATUS_VALUE = "/status/value"
+
+
+class Standalone:
+  
+  # Constructor
+  def __init__(self):
+    
+    
