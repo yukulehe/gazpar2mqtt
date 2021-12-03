8154f47204e9dc6fbaf8a3300477423d66bf0042
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-12-03 16:11:58 +0100

    Update gazpar2mqtt.py

--- app/gazpar2mqtt.py
+++ app/gazpar2mqtt.py
@@ -30,23 +30,7 @@ GRDF_API_ERRONEOUS_COUNT = 1 # Erroneous number of results send by GRDF
 
 # Sensors topics for standalone mode
 
-## Daily
-TOPIC_DAILY_DATE = "/daily/date"
-TOPIC_DAILY_KWH = "/daily/kwh"
-TOPIC_DAILY_MCUBE = "/daily/mcube"
 
-## Monthly
-TOPIC_MONTHLY_DATE = "/monthly/month"
-TOPIC_MONTHLY_KWH = "/monthly/kwh"
-TOPIC_MONTHLY_KWH_TSH = "/monthly/kwh/threshold"
-TOPIC_MONTHLY_KWH_PREV = "/monthly/kwh/previous"
-TOPIC_MONTHLY_MCUBE = "/monthly/mcube"
-TOPIC_MONTHLY_MCUBE_PREV = "/monthly/kwh/previous"
-
-
-## Status
-TOPIC_STATUS_DATE = "/status/date"
-TOPIC_STATUS_VALUE = "/status/value"
 
 # Hass global
 HASS_AUTODISCOVERY_PREFIX = None
