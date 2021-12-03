63ca2caf49f2b13dd3d1272ceced25f6d88f68a3
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-12-03 16:20:27 +0100

    Update standalone.py

--- app/standalone.py
+++ app/standalone.py
@@ -6,15 +6,7 @@
 TOPIC_DAILY_DATE = "/daily/date"
 TOPIC_DAILY_KWH = "/daily/kwh"
 TOPIC_DAILY_MCUBE = "/daily/mcube"
-
-## Monthly
-TOPIC_MONTHLY_DATE = "/monthly/month"
-TOPIC_MONTHLY_KWH = "/monthly/kwh"
-TOPIC_MONTHLY_KWH_TSH = "/monthly/kwh/threshold"
-TOPIC_MONTHLY_KWH_PREV = "/monthly/kwh/previous"
-TOPIC_MONTHLY_MCUBE = "/monthly/mcube"
-TOPIC_MONTHLY_MCUBE_PREV = "/monthly/kwh/previous"
-
+TOPIC_DAILY_INDEX = "/daily/index"
 
 ## Status
 TOPIC_STATUS_DATE = "/status/date"
@@ -24,6 +16,16 @@ TOPIC_STATUS_VALUE = "/status/value"
 class Standalone:
   
   # Constructor
-  def __init__(self):
+  def __init__(self,prefix):
+    
+    self.prefix = prefix
+    
+    self.statusDateTopic = prefix + TOPIC_STATUS_DATE
+    self.statusValue = prefix + TOPIC_STATUS_VALUE
+    
+    self.daily_date = prefix + TOPIC_DAILY_DATE
+    self.daily_kwh = prefix + TOPIC_DAILY_KWH
+    self.daily_mcube = prefix + TOPIC_DAILY_MCUBE
+    self.daily_index = prefix + TOPIC_DAILY_INDEX
     
     
