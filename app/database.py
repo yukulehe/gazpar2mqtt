221bb5f7fb09bb8d38179e56292cf3c864c3e2cc
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-12-06 17:34:08 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -11,10 +11,60 @@ DATABASE_TIMEOUT = 10
 class Database:
   
   # Constructor
-  def __init__(self):
+  def __init__(self,g2mVersion):
   
     self.con = None
     self.cur = None
+    
+  def init(self):
+    
+    # Create table config
+    logging.debug("Creation of configuration table")
+    cur.execute('''CREATE TABLE config (
+                        key TEXT PRIMARY KEY,
+                        value json NOT NULL)''')
+    cur.execute('''CREATE UNIQUE INDEX idx_config_key
+                    ON config (key)''')
+
+    ## Create table of PCEs
+    logging.debug("Creation of PCEs table")
+    cur.execute('''CREATE TABLE pces (
+                        pce TEXT PRIMARY KEY,
+                        json json NOT NULL, 
+                        count INTEGER)''')
+    cur.execute('''CREATE UNIQUE INDEX idx_pces_pce
+                    ON pces (pce)''')
+
+    # Create table for daily consumptions
+    logging.debug("Creation of daily consumptions table")
+    cur.execute('''CREATE TABLE consumption_daily (
+                        pce TEXT NOT NULL, 
+                        date TEXT NOT NULL, 
+                        value INTEGER NOT NULL)''')
+    cur.execute('''CREATE UNIQUE INDEX idx_date_consumption
+                    ON consumption_daily (date)''')
+    
+    # Create table for billing
+    logging.debug("Creation of billing table")
+    cur.execute('''CREATE TABLE billing (
+                        pce TEXT NOT NULL, 
+                        start_date TEXT NOT NULL,
+                        end_date TEXT NOT NULL,
+                        end_index  INTEGER NOT NULL,
+                        conversion REAL NOT NULL)''')
+    cur.execute('''CREATE UNIQUE INDEX idx_start_date_billing
+                    ON billing (start_date)''')
+
+    # Set default configuration
+    logging.debug("Store default configuration")
+    config_query = f"INSERT OR REPLACE INTO config VALUES (?, ?)"
+    config = {
+        "day": datetime.datetime.now().strftime('%Y-%m-%d'),
+        "version": g2mVersion
+    }
+    logging.info(json.dumps(config))
+    self.cur.execute(config_query, ["config", json.dumps(config)])
+    self.commit()
   
   # Connexion to database
   def connect(self):
@@ -25,7 +75,7 @@ class Database:
         logging.debug("Directory /data created")
     
     # Initialize database if not exists
-    if not os.path.exists('/data/enedisgateway.db'):
+    if not os.path.exists(DATABASE_NAME):
         logging.debug("Initialization of the SQLite database...")
         self.con = sqlite3.connect(DATABASE_NAME, timeout=DATABASE_TIMEOUT)
         self.cur = self.con.cursor()
