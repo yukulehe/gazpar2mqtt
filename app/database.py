6882515c78766a26b65996edb6ea913573a8f7f5
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-12-09 15:27:34 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -34,32 +34,32 @@ class Database:
     ## Create table of PCEs
     logging.debug("Creation of PCEs table")
     self.cur.execute('''CREATE TABLE IF NOT EXISTS pces (
-                        pce TEXT PRIMARY KEY,
-                        json json NOT NULL, 
-                        count INTEGER)''')
+                        pce TEXT PRIMARY KEY
+                        , json json NOT NULL
+                        , count INTEGER)''')
     self.cur.execute('''CREATE UNIQUE INDEX IF NOT EXISTS idx_pces_pce
                     ON pces (pce)''')
 
     # Create table for daily consumptions
     logging.debug("Creation of daily consumptions table")
     self.cur.execute('''CREATE TABLE IF NOT EXISTS consumption_daily (
-                        pce TEXT NOT NULL, 
-                        date TEXT NOT NULL,
-                        index INT8 NOT NULL,
-                        volume INTEGER NOT NULL,
-                        energy INTEGER NOT NULL,
-                        conversion REAL)''')
+                        pce TEXT NOT NULL 
+                        , date TEXT NOT NULL
+                        , index INT8 NOT NULL
+                        , volume INTEGER NOT NULL
+                        , energy INTEGER NOT NULL
+                        , conversion REAL)''')
     self.cur.execute('''CREATE UNIQUE INDEX IF NOT EXISTS idx_date_consumption
                     ON consumption_daily (date)''')
     
     # Create table for billing
     logging.debug("Creation of billing table")
     self.cur.execute('''CREATE TABLE IF NOT EXISTS billing (
-                        pce TEXT NOT NULL, 
-                        start_date TEXT NOT NULL,
-                        end_date TEXT NOT NULL,
-                        end_index  INTEGER NOT NULL,
-                        conversion REAL NOT NULL)''')
+                        pce TEXT NOT NULL 
+                        , start_date TEXT NOT NULL
+                        , end_date TEXT NOT NULL
+                        , end_index  INTEGER NOT NULL
+                        , conversion REAL NOT NULL)''')
     self.cur.execute('''CREATE UNIQUE INDEX IF NOT EXISTS idx_start_date_billing
                     ON billing (start_date)''')
 
