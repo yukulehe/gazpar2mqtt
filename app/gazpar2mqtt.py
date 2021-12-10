d89506f1f27d64d1a21c4bc12861af984b2a3f07
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-12-10 09:58:08 +0100

    Update gazpar2mqtt.py

--- app/gazpar2mqtt.py
+++ app/gazpar2mqtt.py
@@ -65,7 +65,7 @@ def run(myParams):
     
     # STEP 1 : Connect to database
     logging.info("-----------------------------------------------------------")
-    logging.info("Connexion to SQLite database...")
+    logging.info("#        Connexion to SQLite database                     #")
     logging.info("-----------------------------------------------------------")
     
     # Create/Update database
@@ -104,7 +104,7 @@ def run(myParams):
     
     # STEP 2 : Log to MQTT broker
     logging.info("-----------------------------------------------------------")
-    logging.info("------         Connexion to Mqtt broker              ------")
+    logging.info("#              Connexion to Mqtt broker                   #")
     logging.info("-----------------------------------------------------------")
     
     try:
@@ -130,7 +130,7 @@ def run(myParams):
     if myMqtt.isConnected:
     
         logging.info("-----------------------------------------------------------")
-        logging.info("------       Get data from GRDF website              ------")
+        logging.info("#            Get data from GRDF website                    #")
         logging.info("-----------------------------------------------------------")
 
         tryCount = 0
@@ -234,7 +234,7 @@ def run(myParams):
         try:
 
             logging.info("-----------------------------------------------------------")
-            logging.info("------      Stand alone publication mode             ------")
+            logging.info("#           Stand alone publication mode                   #")
             logging.info("-----------------------------------------------------------")
 
             # Loop on PCEs
@@ -342,7 +342,7 @@ def run(myParams):
         #try:
 
             logging.info("-----------------------------------------------------------")
-            logging.info("------      Home assistant publication mode          ------")
+            logging.info("#           Home assistant publication mode               #")
             logging.info("-----------------------------------------------------------")
 
             # Create hass instance
@@ -460,7 +460,7 @@ def run(myParams):
     if myMqtt.isConnected:
         
         logging.info("-----------------------------------------------------------")
-        logging.info("------          Disconnexion from MQTT               ------")
+        logging.info("#               Disconnexion from MQTT                     #")
         logging.info("-----------------------------------------------------------")
         
         try:
@@ -473,7 +473,7 @@ def run(myParams):
     
     # STEP 6 : Disconnect from database
     logging.info("-----------------------------------------------------------")
-    logging.info("------     Disconnexion from SQLite database         ------")
+    logging.info("#          Disconnexion from SQLite database               #")
     logging.info("-----------------------------------------------------------")
       
     if myDb.isConnected() :
@@ -482,7 +482,7 @@ def run(myParams):
     
     # STEP 7 : Display next run info and end of program
     logging.info("-----------------------------------------------------------")
-    logging.info("------                Next run                       ------")
+    logging.info("#                Next run                                  #")
     logging.info("-----------------------------------------------------------")
     if myParams.scheduleTime is not None:
         logging.info("gazpar2mqtt next run scheduled at %s",myParams.scheduleTime)
@@ -502,7 +502,7 @@ def run(myParams):
     myEntity = None
     
     logging.info("-----------------------------------------------------------")
-    logging.info("------             End of program                    ------")
+    logging.info("#                  End of program                         #")
     logging.info("-----------------------------------------------------------")
         
         
@@ -525,7 +525,7 @@ if __name__ == "__main__":
     
     # Say welcome and be nice
     logging.info("-----------------------------------------------------------")
-    logging.info("------          Welcome to gazpar2mqtt               ------")
+    logging.info("#               Welcome to gazpar2mqtt                     #")
     logging.info("-----------------------------------------------------------")
     logging.info("Version " + G2M_VERSION)
     logging.info("Please note that the the tool is still under development, various functions may disappear or be modified.")
@@ -534,7 +534,7 @@ if __name__ == "__main__":
     
     # Log params info
     logging.info("-----------------------------------------------------------")
-    logging.info("------           Program parameters                  ------")
+    logging.info("#                Program parameters                        #")
     logging.info("-----------------------------------------------------------")
     myParams.logParams()
     
