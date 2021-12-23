9c864cd3112d9f3973518a0415f9621a535d899c
yukulehe <31646663+yukulehe@users.noreply.github.com>
2021-12-23 14:30:20 +0100

    Update gazpar.py

--- app/gazpar.py
+++ app/gazpar.py
@@ -590,7 +590,7 @@ class Pce:
             self.tshM1Warn = None
             if self.tshM1 and self.gasM1Y0:
                 if self.tshM1 > 0:
-                    self.tshM1Pct = round((self.tshM1 / self.tshM0)*100)
+                    self.tshM1Pct = round((self.gasM1Y0 / self.tshM1)*100)
                     if self.tshM1Pct > 0.8:
                         self.tshM1Warn = "ON"
                     else:
